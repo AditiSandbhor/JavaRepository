ce2aeeb41bc190f751b3cc916d33bce6be0c0960
AditiSandbhor <114947801+AditiSandbhor@users.noreply.github.com>
2024-10-28 18:07:14 +0530

    Create Narrow_Casting.java

---
+++ JavaPractice/src/JavaPractice/Narrow_Casting.java
@@ -0,0 +1,27 @@
+package JavaPractice;
+
+public class Narrow_Casting {
+
+	public static void main(String[] args) {
+		 // bigger  data type
+        double doubleEx = 15.7;
+
+
+        // Narrow casting double to integer  
+        int intEx = (int) doubleEx;
+
+
+        //printing double value
+        System.out.println(" double value: " + doubleEx);
+
+
+        //printing integer value
+        System.out.println(" Narrowed integer value: " + intEx);
+		
+	}
+
+}
+/*
+double value: 15.7
+Narrowed integer value: 15
+*/
