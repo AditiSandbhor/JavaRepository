cf59a734803969c20ec6bfd0a6480b1b2a44f323
AditiSandbhor <114947801+AditiSandbhor@users.noreply.github.com>
2024-10-28 18:06:16 +0530

    Create Sqr_Peri_Area.java

---
+++ JavaPractice/src/JavaPractice/Sqr_Peri_Area.java
@@ -0,0 +1,29 @@
+package JavaPractice;
+
+
+public class Sqr_Peri_Area {
+
+	
+	public static void main(String[] args) {
+		//Taking side of the square
+		double s1 = 5;
+		
+		//Formula for Perimeter
+		double Perimeter1 = 4*s1;
+		
+		//Formula for area
+		double area1 = s1*s1;
+		
+		//Printing the Perimeter
+		System.out.println("The Perimeter of the square is:"+Perimeter1);
+		
+		//Printing the area
+		System.out.println("The area of the square is:"+area1);		
+		
+	}
+
+}
+/*The Perimeter of the square is:20.0
+The area of the square is:25.0
+*/
+
